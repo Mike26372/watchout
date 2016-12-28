e2a97b366724b605a60806888e0f218d7f915031
Michael Tutt <tutt.michael@gmail.com>
2016-12-28 11:10:36 -0800

    Update svg to adjust to width of page

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,9 @@
 // start slingin' some d3 here.
+var watchout = d3.select('#watchout');
+var width = parseInt(d3.select('div.board').style('width'), 10);
+var height = parseInt(d3.select('div.board').style('height'), 10);
+
+// Set watchout SVG height and width to equal current board
+watchout.style('width', '' + width + 'px').style('height', '' + height + 'px');
+
+var numEnemies = 10;
