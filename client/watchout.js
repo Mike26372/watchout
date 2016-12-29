db72c25445b11a5dea27deef5069740a0506df76
Michael Tutt <tutt.michael@gmail.com>
2016-12-28 19:15:31 -0800

    Create Player class from CoffeeScript backup

--- client/watchout.js
+++ client/watchout.js
@@ -25,8 +25,8 @@ var axes = {
 
 var gameBoard = d3.select('.board')
                 .append('svg:svg')
-                .attr('width', gameOptions.width)
-                .attr('height', gameOptions.height);
+                .attr('width', gameOptions.width + 'px')
+                .attr('height', gameOptions.height + 'px');
 
 var updateScore = function() {
   d3.select('.current span').text(gameStats.score.toString());
@@ -36,3 +36,68 @@ var updateBestScore = function() {
   gameStats.bestScore = _.max([gameStats.bestScore, gameStats.score]);
   d3.select('.highscore span').text(gameStats.bestScore.toString());
 };
+
+class Player {
+  constructor(gameOptions) {
+    this.gameOptions = gameOptions;
+    this.path = 'm-7.5,1.62413c0,-5.04095 4.08318,-9.12413 9.12414,-9.12413c5.04096,0 9.70345,5.53145 11.87586,9.12413c-2.02759,2.72372 -6.8349,9.12415 -11.87586,9.12415c-5.04096,0 -9.12414,-4.08318 -9.12414,-9.12415z';
+    this.fill = '#ff6600';
+    this.x = 0;
+    this.y = 0;
+    this.angle = 0;
+    this.r = 5;
+
+  }
+  render (to) {
+    this.el = to.append('svg:path').attr('d', this.path).attr('fill', this.fill);
+    this.transform ({ x: this.gameOptions.width * 0.5, y: this.gameOptions.height * 0.5 });
+    this.setupDragging();
+    return this;
+    // CHECK IF ANYTHING NEEDS TO BE PASSED TO SETUPDRAGGING
+  }
+  getX () {
+    return this.x;
+  }
+  getY () {
+    return this.y;
+  }
+  setX (x) {
+    var minX = this.gameOptions.padding;
+    var maxX = this.gameOptions.width - this.gameOptions.padding;
+    if (x <= minX) {
+      this.x = minX;
+    }
+    if (x >= maxX) {
+      this.x = maxX;
+    }
+  }
+  setY (y) {
+    var minY = this.gameOptions.padding;
+    var maxY = this.gameOptions.height - this.gameOptions.padding;
+    if (y <= minY) {
+      this.y = minY;
+    }
+    if (y >= maxY) {
+      this.y = maxY;
+    }
+  }
+  transform (opts) {
+
+  }
+  moveAbsolute (x, y) {
+
+  }
+  moveRelative (dx, dy) {
+
+  }
+  setupDragging () {
+    var dragMove = this.moveRelative(d3.event.dx, d3.event.dy);
+    var drag = d3.behavior.drag().on('drag', dragMove);
+    this.el.call(drag);
+  }
+}
+
+
+
+
+
